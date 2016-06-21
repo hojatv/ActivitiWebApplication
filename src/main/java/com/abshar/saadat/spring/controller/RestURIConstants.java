c9780bdbcf647b711ec5ae4f084f91e64b0f91e1
hovaheb <hojat.vaheb@gmail.com>
2016-06-21 16:55:32 +0800

    adding TaskService

--- src/main/java/com/abshar/saadat/spring/controller/RestURIConstants.java
+++ src/main/java/com/abshar/saadat/spring/controller/RestURIConstants.java
@@ -2,4 +2,5 @@ package com.abshar.saadat.spring.controller;
 
 public class RestURIConstants {
     public static final String GET_ALL_USERS = "/rest/users";
+    public static final String GET_TASKS = "/rest/tasks";
 }
