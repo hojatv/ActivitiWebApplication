5433a7f0c64ddc64bd0ec055591bb6351e3b2aff
hovaheb <hojat.vaheb@gmail.com>
2016-06-27 17:11:38 +0800

    Adding Identity Controller

---
+++ src/main/java/com/activiti/abshar/spring/controller/IdentityController.java
@@ -0,0 +1,39 @@
+package com.activiti.abshar.spring.controller;
+
+import com.activiti.abshar.spring.controller.RestURIConstants;
+import com.activiti.abshar.spring.model.UserModel;
+import org.activiti.engine.IdentityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hovaheb on 6/27/2016.
+ */
+@Controller
+public class IdentityController {
+    @Autowired
+    private IdentityService identityService;
+    @RequestMapping(value = RestURIConstants.GET_ALL_USERS, method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<UserModel> getAllUsers() {
+        List<org.activiti.engine.identity.User> users = identityService.createUserQuery().list();
+        List<UserModel> result = new ArrayList<UserModel>();
+        for(org.activiti.engine.identity.User user:users){
+            UserModel userModel = new UserModel();
+            userModel.setEmail(user.getEmail());
+            userModel.setFirstName(user.getFirstName());
+            userModel.setLastName(user.getLastName());
+            userModel.setId(user.getId());
+            userModel.setPassword(user.getPassword());
+            result.add(userModel);
+        }
+        return result;
+    }
+}
