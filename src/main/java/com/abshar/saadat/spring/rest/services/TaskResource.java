3cc9074fa2fdd84a934a9f365b942543c6758416
hovaheb <hojat.vaheb@gmail.com>
2016-06-27 14:39:54 +0800

    Creating process engine from a Spring configuration

--- src/main/java/com/abshar/saadat/spring/rest/services/TaskResource.java
+++ src/main/java/com/abshar/saadat/spring/rest/services/TaskResource.java
@@ -3,10 +3,13 @@ package com.abshar.saadat.spring.rest.services;
 import com.abshar.saadat.spring.controller.RestURIConstants;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.form.TaskFormData;
 import org.activiti.engine.task.Task;
 import org.activiti.rest.service.api.runtime.task.TaskResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,14 +19,19 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Created by hovaheb on 6/21/2016.
  */
+
 @Controller
 public class TaskResource {
+    @Autowired
+    private TaskService taskService;
+
+    @Transactional
     @RequestMapping(value = RestURIConstants.GET_TASKS,params = {"taskId"},method = RequestMethod.GET)
     public
     @ResponseBody
     TaskResponse getTasks(String taskId) {
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        Task task = processEngine.getTaskService()
+        /*ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();*/
+        Task task = taskService
                 .createTaskQuery()
                 .taskId(taskId)
                 .singleResult();
